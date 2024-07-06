34383731d5e3ee724ee3efca1eefdc609104cc28
SmitDatir <141640425+SmitDatir@users.noreply.github.com>
2024-07-06 22:31:39 +0530

    Add files via upload
    Above java code is used to find perimeter of rhombus

---
+++ perimeter_of_rhombus.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class perimeter_of_rhombus {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        int a;
+        System.out.println("Enter the lenght of a rhombus: ");
+        a=sc.nextInt();
+        System.out.println("Perimeter Of Rhombus is "+(4*a));
+    }
+}
